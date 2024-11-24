9667562edccfcaae2a720c273aa6731b0951e249
Alex Erdei <mralexerdei@yahoo.co.uk>
2024-11-24 21:54:09 +0000

    Make post with AINIRO

--- backend/backend.js
+++ backend/backend.js
@@ -33,21 +33,19 @@ export async function login(username, password) {
 }
 
 export async function updatePost(post, token) {
-	const response = await fetch(`${BASE_URL}/posts/${post._id}`, {
-		method: "PUT",
-		mode: "cors",
-		headers: {
-			"Content-Type": "application/json",
-			Authorization: token,
-		},
-		body: JSON.stringify(post),
-	});
-	return await getJSON(response);
-}
-
-export async function updateBlock(block, token) {
+	console.log(post);
+	const { _id, author, content, likes, title } = { ...post };
+	console.log(_id);
+	const payload = {
+		post_id: _id,
+		author: author._id,
+		content: JSON.stringify(content.map((block) => block._id)),
+		likes,
+		title,
+	};
+	console.log(payload);
 	const response = await fetch(
-		`${BASE_URL}/posts/${block.post}/blocks/${block._id}`,
+		`https://alexerdei-team.us.ainiro.io/magic/modules/blog-api/posts`,
 		{
 			method: "PUT",
 			mode: "cors",
@@ -55,7 +53,33 @@ export async function updateBlock(block, token) {
 				"Content-Type": "application/json",
 				Authorization: token,
 			},
-			body: JSON.stringify(block),
+			body: JSON.stringify(payload),
+		}
+	);
+	const json = response.json();
+	const res = {
+		errors: [],
+		success: true,
+		post: json,
+	};
+	return res;
+}
+
+export async function updateBlock(block, token) {
+	const payload = { ...block };
+	payload.block_id = payload._id;
+	delete payload._id;
+	delete payload.errors;
+	const response = await fetch(
+		`https://alexerdei-team.us.ainiro.io/magic/modules/blog-api/blocks`,
+		{
+			method: "PUT",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: token,
+			},
+			body: JSON.stringify(payload),
 		}
 	);
 	return await getJSON(response);
@@ -77,20 +101,62 @@ export async function getPost(ID) {
 	//AINIRO returns null for empty array
 	if (!json.content) json.content = [];
 	if (!json.likes) json.likes = [];
+	//AINIRO uses post_id instead of _id
+	json._id = json.post_id;
 	return json;
 }
 
 export async function createBlock(block, token) {
-	const response = await fetch(`${BASE_URL}/posts/${block.post}/blocks`, {
-		method: "POST",
-		mode: "cors",
-		headers: {
-			"Content-Type": "application/json",
-			Authorization: token,
-		},
-		body: JSON.stringify(block),
-	});
-	return await getJSON(response);
+	const res = {
+		errors: block.errors,
+		block: block,
+		success: false,
+	};
+	const payload = { ...block };
+	delete payload.errors;
+	const response = await fetch(
+		"https://alexerdei-team.us.ainiro.io/magic/modules/blog-api/blocks",
+		{
+			method: "POST",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: token,
+			},
+			body: JSON.stringify(payload),
+		}
+	);
+	const json = await response.json();
+	console.log(json);
+	if (json.id) {
+		res.success = true;
+		res.block._id = json.id;
+		console.log(block.post);
+		const post = await getPost(block.post);
+		console.log(post.content);
+		post.content.push(json.id);
+		console.log(post.content);
+		//post.content = JSON.stringify(post.content);
+		console.log(post);
+		const { author, type, post_id, content, likes } = post;
+		console.log(
+			await putPost(
+				{
+					author,
+					type,
+					content,
+					likes,
+					post_id,
+				},
+				token
+			)
+		);
+		console.log(res);
+		return res;
+	} else {
+		res.errors.push[{ msg: json.message }];
+		return res;
+	}
 }
 
 export async function deleteBlock(block, token) {
@@ -116,6 +182,7 @@ export async function getPosts() {
 		},
 	});
 	const json = await response.json();
+	console.log(json);
 	return json;
 }
 
@@ -139,6 +206,8 @@ export async function postPosts(post, token) {
 	if (json.id) {
 		json.success = true;
 		json.post = await getPost(json.id);
+		json._id = json.post_id;
+		delete json.post_id;
 	}
 	return json;
 }
@@ -190,6 +259,25 @@ export async function updatePostLikes(postId, userId) {
 		},
 		body: JSON.stringify({ user: userId }),
 	});
+	const json = await response.json();
+	return json;
+}
+
+async function putPost(post, token) {
+	post.likes = JSON.stringify(post.likes);
+	post.content = JSON.stringify(post.content);
+	const response = await fetch(
+		"https://alexerdei-team.us.ainiro.io/magic/modules/blog-api/posts",
+		{
+			method: "PUT",
+			mode: "cors",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: token,
+			},
+			body: JSON.stringify(post),
+		}
+	);
 	const json = await response.json();
 	return json;
 }
--- components/editBlock.js
+++ components/editBlock.js
@@ -5,117 +5,117 @@ import { submitBlock } from "../index.js";
 import { removeBlock, swapBlocks } from "./post.js";
 
 function numberOfLines(text) {
-  return text.split("\n").length;
+	return text.split("\n").length;
 }
 
 function textWithLinks(block) {
-  if (!block.links) return block.text;
-  const text = [];
-  let previousPosition = 0;
-  block.links.forEach((link) => {
-    const nextTextPiece = block.text.slice(previousPosition, link.position);
-    if (nextTextPiece) text.push(nextTextPiece);
-    text.push(`[${link.description}](${link.url})`);
-    previousPosition = link.position;
-  });
-  const nextTextPiece = block.text.slice(previousPosition, block.text.length);
-  if (nextTextPiece) text.push(nextTextPiece);
-  return text.join("");
+	if (!block.links) return block.text;
+	const text = [];
+	let previousPosition = 0;
+	block.links.forEach((link) => {
+		const nextTextPiece = block.text.slice(previousPosition, link.position);
+		if (nextTextPiece) text.push(nextTextPiece);
+		text.push(`[${link.description}](${link.url})`);
+		previousPosition = link.position;
+	});
+	const nextTextPiece = block.text.slice(previousPosition, block.text.length);
+	if (nextTextPiece) text.push(nextTextPiece);
+	return text.join("");
 }
 
 export function initEditBlock(block) {
-  const editBlockNode = importTemp(20);
-  editBlockNode.setAttribute("data-blockid", block._id);
-  const cancelButton = editBlockNode.querySelector(".buttons .cancel");
-  const submitButton = editBlockNode.querySelector(".buttons .submit");
-  const swapButton = editBlockNode.querySelector(".buttons .swap");
-  if (!isNaN(block._id)) submitButton.classList.add("hidden");
-  const inputNode = importTemp(21);
-  const textareaNode = editBlockNode.querySelector(".input-container textarea");
-  const selectTypeNode = editBlockNode.querySelector("select#type");
-  selectTypeNode.value = block.type;
-  if (block.type !== "subtitle") {
-    inputNode.addEventListener("change", function () {
-      textareaNode.value = inputNode.value;
-    });
-    textareaNode.addEventListener("change", function () {
-      inputNode.value = textareaNode.value;
-    });
-    textareaNode.style = "";
-    if (block.type === "code") {
-      const selectLangNode = editBlockNode.querySelector("#language");
-      selectLangNode.classList.remove("hidden");
-      selectLangNode.value = block.language;
-      textareaNode.value = block.text;
-      textareaNode.rows = numberOfLines(block.text);
-    } else {
-      textareaNode.value = textWithLinks(block);
-    }
-  } else {
-    inputNode.value = block.text;
-    textareaNode.parentNode.replaceChild(inputNode, textareaNode);
-  }
-  cancelButton.addEventListener("click", function () {
-    if (block.errors && block.errors.length !== 0) {
-      if (!isNaN(block._id)) removeBlock(block);
-      else delete block.errors;
-    }
-    const blockNode = initBlock(block, true);
-    editBlockNode.parentNode.replaceChild(blockNode, editBlockNode);
-    if (block.type === "code") prism();
-    showSwapButton();
-  });
-  editBlockNode.addEventListener("submit", function (event) {
-    event.preventDefault();
-    submitBlock(getNewBlock(block, editBlockNode));
-  });
-  swapButton.addEventListener("click", function () {
-    swapBlocks();
-  });
-  const blockNode = document.querySelector(`[data-blockid="${block._id}"]`);
-  insertErrorMessages(editBlockNode, block.errors);
-  blockNode.parentNode.replaceChild(editBlockNode, blockNode);
+	const editBlockNode = importTemp(20);
+	editBlockNode.setAttribute("data-blockid", block._id);
+	const cancelButton = editBlockNode.querySelector(".buttons .cancel");
+	const submitButton = editBlockNode.querySelector(".buttons .submit");
+	const swapButton = editBlockNode.querySelector(".buttons .swap");
+	//if (!isNaN(block._id)) submitButton.classList.add("hidden");
+	const inputNode = importTemp(21);
+	const textareaNode = editBlockNode.querySelector(".input-container textarea");
+	const selectTypeNode = editBlockNode.querySelector("select#type");
+	selectTypeNode.value = block.type;
+	if (block.type !== "subtitle") {
+		inputNode.addEventListener("change", function () {
+			textareaNode.value = inputNode.value;
+		});
+		textareaNode.addEventListener("change", function () {
+			inputNode.value = textareaNode.value;
+		});
+		textareaNode.style = "";
+		if (block.type === "code") {
+			const selectLangNode = editBlockNode.querySelector("#language");
+			selectLangNode.classList.remove("hidden");
+			selectLangNode.value = block.language;
+			textareaNode.value = block.text;
+			textareaNode.rows = numberOfLines(block.text);
+		} else {
+			textareaNode.value = textWithLinks(block);
+		}
+	} else {
+		inputNode.value = block.text;
+		textareaNode.parentNode.replaceChild(inputNode, textareaNode);
+	}
+	cancelButton.addEventListener("click", function () {
+		if (block.errors && block.errors.length !== 0) {
+			if (!isNaN(block._id)) removeBlock(block);
+			else delete block.errors;
+		}
+		const blockNode = initBlock(block, true);
+		editBlockNode.parentNode.replaceChild(blockNode, editBlockNode);
+		if (block.type === "code") prism();
+		showSwapButton();
+	});
+	editBlockNode.addEventListener("submit", function (event) {
+		event.preventDefault();
+		submitBlock(getNewBlock(block, editBlockNode));
+	});
+	swapButton.addEventListener("click", function () {
+		swapBlocks();
+	});
+	const blockNode = document.querySelector(`[data-blockid="${block._id}"]`);
+	insertErrorMessages(editBlockNode, block.errors);
+	blockNode.parentNode.replaceChild(editBlockNode, blockNode);
 }
 
 function getNewBlock(block, node) {
-  const inputNode = node.querySelector("input");
-  const textareaNode = node.querySelector("textarea");
-  const selectTypeNode = node.querySelector("select#type");
-  const selectLangNode = node.querySelector("select#language");
-  const newBlock = { ...block };
-  newBlock.type = selectTypeNode.value;
-  if (block.type !== "subtitle") newBlock.text = textareaNode.value;
-  else newBlock.text = inputNode.value;
-  newBlock.language = " ";
-  if (block.type === "code") newBlock.language = selectLangNode.value;
-  const output = separateLinksFromText(newBlock.text);
-  newBlock.text = output.text;
-  newBlock.links = output.links;
-  return newBlock;
+	const inputNode = node.querySelector("input");
+	const textareaNode = node.querySelector("textarea");
+	const selectTypeNode = node.querySelector("select#type");
+	const selectLangNode = node.querySelector("select#language");
+	const newBlock = { ...block };
+	newBlock.type = selectTypeNode.value;
+	if (block.type !== "subtitle") newBlock.text = textareaNode.value;
+	else newBlock.text = inputNode.value;
+	newBlock.language = " ";
+	if (block.type === "code") newBlock.language = selectLangNode.value;
+	const output = separateLinksFromText(newBlock.text);
+	newBlock.text = output.text;
+	newBlock.links = output.links;
+	return newBlock;
 }
 
 function separateLinksFromText(textInput) {
-  const textPieces = textInput.split("](");
-  const links = [];
-  let position = 0;
-  textPieces.forEach((piece, index) => {
-    const previousPiece = textPieces[index - 1];
-    if (previousPiece && previousPiece.indexOf("[") !== -1) {
-      const pieces = previousPiece.split("[");
-      textPieces[index - 1] = pieces[0];
-      position += pieces[0].length;
-      links.push({ description: pieces[1] });
-    }
-    if (piece.indexOf(")") !== -1) {
-      const pieces = piece.split(")");
-      const link = links.pop();
-      if (link) {
-        link.url = pieces[0];
-        link.position = position;
-        links.push(link);
-        textPieces[index] = pieces[1];
-      }
-    }
-  });
-  return { links: links, text: textPieces.join("") };
+	const textPieces = textInput.split("](");
+	const links = [];
+	let position = 0;
+	textPieces.forEach((piece, index) => {
+		const previousPiece = textPieces[index - 1];
+		if (previousPiece && previousPiece.indexOf("[") !== -1) {
+			const pieces = previousPiece.split("[");
+			textPieces[index - 1] = pieces[0];
+			position += pieces[0].length;
+			links.push({ description: pieces[1] });
+		}
+		if (piece.indexOf(")") !== -1) {
+			const pieces = piece.split(")");
+			const link = links.pop();
+			if (link) {
+				link.url = pieces[0];
+				link.position = position;
+				links.push(link);
+				textPieces[index] = pieces[1];
+			}
+		}
+	});
+	return { links: links, text: textPieces.join("") };
 }
